0fc529e4fbe115772f72ea2fd40d7658baa6aa14
Md. Rashedul Hasan Safa <rashedul.safa@bjitgroup.com>
2023-06-13 17:01:50 +0600

    added button component

---
+++ src/components/atom/button/button.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
+import { Text } from "../text/text";
+import { Icon } from "../icon/icon";
+// import { Icon } from "../icon/icon";
+
+const meta = {
+  title: "Atom/Button",
+  component: Button,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LargePrimary: Story = {
+  args: {
+    mode: "primary",
+    size: "large",
+    children: <Text size={24}>Start Registration</Text>,
+  },
+};
+
+export const MediumPrimary: Story = {
+  args: {
+    mode: "primary",
+    size: "medium",
+    children: <Text>Registration</Text>,
+  },
+};
+
+export const SmallSecondary: Story = {
+  args: {
+    mode: "secondary",
+    size: "small",
+    children: <Text>Register</Text>,
+  },
+};
+
+export const IconButton: Story = {
+  args: {
+    icon: true,
+    children: <Icon icon="left_arrow" height={20} width={10} />,
+  },
+};
---
+++ src/components/atom/button/button.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./button.scss";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  mode?: "primary" | "secondary";
+  icon?: boolean;
+  size?: "small" | "large" | "medium";
+  onClick?: () => void;
+}
+
+export const Button: (props: ButtonProps) => JSX.Element = ({
+  children,
+  mode,
+  icon = false,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      type="button"
+      className={[
+        "a-button",
+        `${icon ? "a-button--icon" : ""}`,
+        `${size ? "a-button--" + size : ""}`,
+        `${mode ? "a-button--" + mode : ""}`,
+      ].join(" ")}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
